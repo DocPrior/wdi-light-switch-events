2950af48bedc90715f150046e3e766fd455fa8c0
DocPrior <Sara Prior>
2017-07-06 15:02:08 -0400

    able to change light to dark

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+  var lightSwitch = document.querySelector('button');
 
+  var docBody = document.querySelector('body');
+
+  lightSwitch.addEventListener('click', function() {
+    docBody.classList.remove('light');
+    docBody.classList.add('dark');
+  })
+
+});
